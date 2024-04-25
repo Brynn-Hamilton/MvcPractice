f1a1dae7b56b92547835f12d42f52c49df6ba7d7
Brynn Hamilton <brynn.s.hamilton@gmail.com>
2024-04-24 20:30:39 -0400

    Add project files.

---
+++ ModelDemo/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelDemo.Models;
+using System.Diagnostics;
+
+namespace ModelDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Sample()
+        {
+            Person fred = new Person(){FirstName = "Fred", LastName = "Franklin", Email = "Frankief@abc.com", BirthYear = 1976 };
+
+            return View(fred);
+        }
+
+        public IActionResult SampleList()
+        {
+            List<Person> people = new List<Person>();
+            Person person = new Person("Fred", "Franklin", "Frankief@abc.com", 1976);
+            people.Add(person);
+            person = new Person("Sally", "Smith", "sally@abc.com", 1994);
+            people.Add(person);
+            person = new Person("Julia", "Jones", "jj18@abc.com", 2012);
+            people.Add(person);
+
+            ViewData["Department"] = "IT";
+            ViewData["Year"] = 2023;
+
+            return View(people);
+        }
+
+        public IActionResult ShowRegister()
+        {
+            return View();
+        }
+
+        public IActionResult Register(Person person)
+        {
+            ViewBag.BornMessage = "Hi";
+
+            return View(person);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelDemo/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace ModelDemo.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int BirthYear { get; set; }
+
+        public Person(string firstName, string lastName, string email, int birthYear)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthYear = birthYear;
+        }
+
+        public Person()
+        {
+            
+        }
+    }
+}
